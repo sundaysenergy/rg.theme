7ec5e3e2d60238b0fe72c923905f22c25b08190f
Tim Lupfer <tim@sundaysenergy.com>
2013-12-09 13:01:35 -0600

    Debugging

--- productview.js
+++ productview.js
@@ -63,6 +63,7 @@ $(document).ready(function() {
       }
       // If our position plus the size of the page is greater than length, we're showing the last entries
       if ((parseInt(productlist.i) + parseInt(productlist.page)) > productlist.matchingItems.length) {
+        console.log(productlist.page, productlist.page-1, productlist.i);
         if (productlist.page != 3) {
           $('.next').addClass('disabled').off('click touch');
         } else if (productlist.i == (productlist.page-1)) {
