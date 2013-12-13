d7f2903694362a2936b33e2d45ec3c2d1d50ef50
Tim Lupfer <tim@sundaysenergy.com>
2013-12-13 15:20:45 -0600

    Landing page

--- landing.js
+++ landing.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
+  var item_template;
+  $.ajax({
+    url: "http://rg.cape.io/templates/landing.html",
+    context: document.body,
+    async: false,
+    error:  function (jqXHR, textStatus, errorThrown) {
+              console.log(errorThrown);
+            }
+  }).done(function(data) {
+    item_template = Hogan.compile(data);
+  });
   $.getJSON('http://rg.cape.io/beautyshots/index.json', function(data) {
-    $.when(_.forEach(data, function(slide) {
-      $('.carousel-inner').append('<div class="item"><img style="width:100%" src="' + slide.img + '"></div>');
-    })).then(function() {
+    _.forEach(data, function(item) {
+      item.pos = function() {
+        return _.findIndex(data, {img:item.img});  
+      }
+    });
+    $.when($('.carousel').append(item_template.render({slides:data}))).then(function() {
       $('.carousel-inner').css('height', '500px');
       $('.carousel-inner .item:nth-of-type(1)').addClass('active');
+      $('.carousel-indicators li:nth-of-type(1)').addClass('active');
       $('.carousel').carousel();
     });
   });
