1b2b77cbed88fd553175d440eee358e4b5f452fe
Tim Lupfer <tim@sundaysenergy.com>
2013-12-13 17:57:36 -0600

    Reset on window resize

--- landing.js
+++ landing.js
@@ -10,32 +10,35 @@ $(document).ready(function() {
   }).done(function(data) {
     item_template = Hogan.compile(data);
   });
-  $.getJSON('http://rg.cape.io/beautyshots/index.json', function(data) {
-    var section = parseInt($(window).width()/5);
-    _.forEach(data, function(item) {
-      item.pos = function() {
-        return _.findIndex(data, {img:item.img});  
-      }
-      for (var i=1;i<=5;i++) {
-        item.items[i-1].leftc = section * (parseInt(i)-1);
-        item.items[i-1].rightc = (section * (parseInt(i)))-1;
-      }
-    });
-    $.when($('.carousel').append(item_template.render({slides:data}))).then(function() {
-      $('.carousel-inner').css('height', '500px');
-      $('.carousel-inner .item:nth-of-type(1)').addClass('active');
-      $('.carousel-indicators li:nth-of-type(1)').addClass('active');
-      $('.carousel').carousel();
-      $("area").hover(function() {
-          // var altText = $(this).data("id");
-          // $('.item-hover').html("Hovering " + altText);
-      }, function() {
-          // $('.item-hover').html();
-      })
-      .click(function() {
-        var itemid = $(this).data("id");
-        hash.add(detailedview:itemid);
+  $(window).on('resize', function() {
+    $.getJSON('http://rg.cape.io/beautyshots/index.json', function(data) {
+      var section = parseInt($(window).width()/5);
+      _.forEach(data, function(item) {
+        item.pos = function() {
+          return _.findIndex(data, {img:item.img});  
+        }
+        for (var i=1;i<=5;i++) {
+          item.items[i-1].leftc = section * (parseInt(i)-1);
+          item.items[i-1].rightc = (section * (parseInt(i)))-1;
+        }
+      });
+      $.when($('.carousel').append(item_template.render({slides:data}))).then(function() {
+        $('.carousel-inner').css('height', '500px');
+        $('.carousel-inner .item:nth-of-type(1)').addClass('active');
+        $('.carousel-indicators li:nth-of-type(1)').addClass('active');
+        $('.carousel').carousel();
+        $("area").hover(function() {
+            // var altText = $(this).data("id");
+            // $('.item-hover').html("Hovering " + altText);
+        }, function() {
+            // $('.item-hover').html();
+        })
+        .click(function() {
+          var itemid = $(this).data("id");
+          hash.add({detailedview:itemid});
+        });
       });
     });
   });
+  $(window).trigger('resize');
 });
