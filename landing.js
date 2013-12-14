284113bd073399f595c7ca5a3a5113f97df4a4b8
Tim Lupfer <tim@sundaysenergy.com>
2013-12-13 19:37:19 -0600

    no message

--- landing.js
+++ landing.js
@@ -36,5 +36,28 @@ $(document).ready(function() {
       });
     });
   });
-  $(window).trigger('resize');
+  $.getJSON('http://rg.cape.io/beautyshots/index.json', function(data) {
+    var section = parseInt($(window).width()/5);
+    _.forEach(data, function(item) {
+      item.pos = function() {
+        return _.findIndex(data, {img:item.img});  
+      }
+      for (var i=1;i<=5;i++) {
+        item.items[i-1].leftc = section * (parseInt(i)-1);
+        item.items[i-1].rightc = (section * (parseInt(i)))-1;
+      }
+    });
+    data[_.random(0,data.length)].active = true;
+    $.when($('.carousel').html(item_template.render({slides:data}))).then(function() {
+      $('.carousel-inner').css('height', '500px');
+      $('.carousel').carousel();
+      $("area").hover(function() {
+        var itemid = $(this).data("id");
+        $('.popover-'+itemid).show();
+      }, function() {
+        var itemid = $(this).data("id");
+        $('.popover-'+itemid).hide();
+      });
+    });
+  });
 });
