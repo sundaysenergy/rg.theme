49819e1884b3ac65cf952df35cabae251110f766
Tim Lupfer <tim@sundaysenergy.com>
2013-12-15 17:06:48 -0600

    Add css and js for search box exposure

--- rg.js
+++ rg.js
@@ -1,1 +1,11 @@
 /* some stuff here */
+$('.fa-search').hover(function() {
+  $('#search-items').slideToggle();
+  $('#search-items').on('submit', function(e) {
+    e.preventDefault();
+    window.location = '/collection.html#search=' + $('#search-items').find('input').val();
+  });
+  $('.fa-search').off('mouseenter');
+}, function() {
+
+});
