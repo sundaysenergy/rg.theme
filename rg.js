137231761f567b375ebb5bc145aa80c28f5280a9
Tim Lupfer <tim@sundaysenergy.com>
2013-12-15 21:02:29 -0600

    s/textiles/textile/

--- rg.js
+++ rg.js
@@ -3,7 +3,7 @@ $('.fa-search').hover(function() {
   $('#search-items').slideToggle();
   $('#search-items').on('submit', function(e) {
     e.preventDefault();
-    window.location = '/collection.html#collection=textiles&search=' + encodeURIComponent($('#search-items').find('input').val());
+    window.location = '/collection.html#collection=textile&search=' + encodeURIComponent($('#search-items').find('input').val());
   });
   $('.fa-search').off('mouseenter');
 }, function() {
