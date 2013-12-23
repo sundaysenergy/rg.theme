2bae0db62220e275dad3e2db5368481ef3bfbb35
Tim Lupfer <tim@timlupfer.com>
2013-12-23 14:45:46 -0600

    Give user feedback on login status

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('form').on('submit', function(e) {
+    var template = Hogan.compile('<div style="position:fixed; top:40%; left:35%; width: 30%;" class="alert alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>{{message}}</div>');
     e.preventDefault();
     var username = $('input[type=email]').val();
     var password = $('input[type=password]').val();
@@ -10,7 +11,11 @@ $(document).ready(function() {
                           }, 
                           function(data, obj, res) {
                             $.cookie('token', data.token, { expires: 7, path: '/', domain: '.rg.cape.io' });
-                            console.log($.cookie('token'));
+                            if (data.token == false) {
+                              $('body').append(template.render({message:'Login succeed. Your token has been stored as a cookie!'}));
+                            } else {
+                              $('body').append(template.render({message:'Login failed.'}));
+                            }
                           });
   });
 });
