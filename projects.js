91ee901fde604e9d3e8ed09e206b7fe3adc4044f
Tim Lupfer <tim@timlupfer.com>
2014-01-02 15:36:13 -0600

    Use a template for project list items and add some comments

--- projects.js
+++ projects.js
@@ -1,65 +1,74 @@
 $(document).ready(function() {
   // If we don't have an authentication token, redirect to the login pag 
   if (_.isUndefined($.cookie('token'))) window.location = '/trade/login.html#destination=' + encodeURIComponent(window.location.pathname);
-  // Get a list of lists and populate the existing-projects
-  $.getJSON('http://rg.cape.io/_api/items/_index/user_list/' + $.cookie('uid') + '/?data_only=true', {}, function(data) {
-    _.forEach(data, function(list) {
-      $('ul.existing-projects').append('<li id="' + list._id + '"><span class="list-name">' + list.info.name + '</span><form style="display:none"><input type="text" value="' + list.info.name + '"></form><button class="plain edit-list"><i class="fa fa-pencil"></i></button><button class="delete-list" style="position: absolute; right: 0">&times;</button></li>');
-
-      $('#'+list._id+' button.edit-list').on('click touch', function(e) {
-        e.preventDefault();
-        $(this).siblings('form').show().on('submit', function(ev) {
-          ev.preventDefault();
-          console.log("Change name " + $(this).find('input[type=text]').val());
+  // Fetch our project list item template
+  $.ajax({ url: "http://rg.cape.io/templates/mini/project_list.html" })
+  .done(function(project_list) {
+    // Compile template
+    var template = Hogan.compile(project_list);
+    // Retrieve the list of lists
+    $.getJSON('http://rg.cape.io/_api/items/_index/user_list/' + $.cookie('uid') + '/?data_only=true', {}, function(data) {
+      // Process each of the lists
+      _.forEach(data, function(list) {
+        $('ul.existing-projects').append(template.render(list));
+        // Change the name of a list
+        $('#'+list._id+' button.edit-list').on('click touch', function(e) {
+          e.preventDefault();
+          $(this).siblings('form').show().on('submit', function(ev) {
+            ev.preventDefault();
+            console.log("Change name " + $(this).find('input[type=text]').val());
+          });
+          $(this).siblings('.list-name').hide();
+          $(this).hide();
         });
-        $(this).siblings('.list-name').hide();
-        $(this).hide();
-      });
-      $('#'+list._id+' button.delete-list').on('click touch', function(e) {
-        e.preventDefault();
-        console.log("Delete "+list._id);
-        $('#'+list._id).remove();
-        $.ajax({
-          url: 'http://rg.cape.io/_api/items/_index/list/'+list._id,
-          type: 'DELETE',
-          success: function(result) {
-            $('#'+list._id).remove();
-            console.log(result);
-          },
-          fail: function(result) {
-            console.log(result);
-          }
-        });
-      });
-    });
-    var sortable = new Sortable($('.existing-projects')[0]);
-  });
-  // Things to do to create a new project
-  $('#project-new').on('click touch', function(e) {
-    $('.new-project').show();
-    $('#new-project-name').closest('form').on('submit', function(e) {
-      e.preventDefault();
-      var projectname = $('#new-project-name').val();
-      var token = 'bearer ' + $.cookie('token');
-      var $div = $(this).closest('div');
-      // If we have a token and a project name, submit the project
-      if ((_.isUndefined(token) == false) && (projectname.length > 0)) {
-        $.ajax({
-          url: 'http://rg.cape.io/_api/items/_index/list',
-          type: 'post',
-          data: { info: { name:projectname } },
-          headers: { Authorization: token },
-          dataType: 'json',
-          success: function (data) {
-            // If the list was created, remove the form and show confirmation
-            if (_.isUndefined(data._id) == false) {
-              $div.find('form').remove();
-              $div.append('List created with id of ' + data._id);
+        // Remove a list
+        $('#'+list._id+' button.delete-list').on('click touch', function(e) {
+          e.preventDefault();
+          console.log("Delete "+list._id);
+          $('#'+list._id).remove();
+          $.ajax({
+            url: 'http://rg.cape.io/_api/items/_index/list/'+list._id,
+            type: 'DELETE',
+            success: function(result) {
+              $('#'+list._id).remove();
+              console.log(result);
+            },
+            fail: function(result) {
+              console.log(result);
             }
-            console.log(data);
-          }
+          });
         });
-      }
+      });
+      // Once we have all of the items, make them sortable
+      var sortable = new Sortable($('.existing-projects')[0]);
+    });
+    // Things to do to create a new project
+    $('#project-new').on('click touch', function(e) {
+      $('.new-project').show();
+      $('#new-project-name').closest('form').on('submit', function(e) {
+        e.preventDefault();
+        var projectname = $('#new-project-name').val();
+        var token = 'bearer ' + $.cookie('token');
+        var $div = $(this).closest('div');
+        // If we have a token and a project name, submit the project
+        if ((_.isUndefined(token) == false) && (projectname.length > 0)) {
+          $.ajax({
+            url: 'http://rg.cape.io/_api/items/_index/list',
+            type: 'post',
+            data: { info: { name:projectname } },
+            headers: { Authorization: token },
+            dataType: 'json',
+            success: function (data) {
+              // If the list was created, remove the form and show confirmation
+              if (_.isUndefined(data._id) == false) {
+                $div.find('form').remove();
+                $div.append('List created with id of ' + data._id);
+              }
+              console.log(data);
+            }
+          });
+        }
+      });
     });
   });
-});
+}); 
