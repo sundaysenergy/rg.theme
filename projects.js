030e6abe2cf668262609163ae199eb18224ccf2b
Tim Lupfer <tim@timlupfer.com>
2014-01-03 16:37:39 -0600

    Add content type to put

--- projects.js
+++ projects.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
             url: 'http://rg.cape.io/_api/items/_index/' + $.cookie('uid') + '/list',
             type: 'PUT',
             data: JSON.stringify(obj),
-            headers: { Authorization: token },
+            headers: { Authorization: token, Content-Type: 'application/json' },
             success: function(result) {
               //location.reload();
               console.log(result);
