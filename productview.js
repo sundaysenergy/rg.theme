279d776b9d33c94121b1c0dbb5d49766f3cb5d16
Tim Lupfer <tim@timlupfer.com>
2014-01-13 14:56:03 -0600

    Debugging

--- productview.js
+++ productview.js
@@ -74,7 +74,9 @@ $(document).ready(function() {
       if (_.isUndefined(hash.get('color'))) {
         $('.filter-color').find(':checkbox').attr('checked',false);
       }
+      console.log(productlist);
       if ($('button.thumbs').is(":visible") && productlist.page == rg_options.vertical_page) {
+        console.log("here");
         $('button.thumbs').trigger('click');
         $('button.slide').trigger('click');
       }
