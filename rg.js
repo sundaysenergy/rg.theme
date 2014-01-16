4391beb2227a211213ff6b4b1c73e1bd08501f67
Tim Lupfer <tim@timlupfer.com>
2014-01-16 13:57:24 -0600

    [#63997912] Default to textile, but don’t force it if there is another collection present in the url

--- rg.js
+++ rg.js
@@ -3,7 +3,9 @@ $('.fa-search').hover(function() {
   $('#search-items').slideToggle();
   $('#search-items').on('submit', function(e) {
     e.preventDefault();
-    window.location = '/collection.html#collection=textile&search=' + encodeURIComponent($('#search-items').find('input').val());
+    var collection = hash.get('collection');
+    if (_.isUndefined(collection)) collection = 'textile';
+    window.location = '/collection.html#collection='+collection+'&search=' + encodeURIComponent($('#search-items').find('input').val());
   });
   $('.fa-search').off('mouseenter');
 }, function() {
