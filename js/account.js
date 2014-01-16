f048ea7f5a60e870af7fbfeac5fb8c8e66ec3c3a
Tim Lupfer <tim@timlupfer.com>
2014-01-16 10:15:20 -0600

    Render the template

--- js/account.js
+++ js/account.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
   $.ajax({ url: "http://rg.cape.io/templates/mini/account.html" })
   .done(function(template) {
-    console.log("step #1");
+    var account_info = Hogan.compile(template);
     $.ajax({ url: '/_api/db/_entity/user/'+$.cookie('uid')+'/profile.json'})
     .done(function(user_info) {
-      console.log(user_info);
-      console.log(template);
+      $('.account-information').html(account_info.render(user_info));
     });
   });
 });
