b7be964c78faf898f300f3daab7796867513c47e
Tim Lupfer <tim@timlupfer.com>
2014-01-16 09:56:18 -0600

    Fix typo

--- js/account.js
+++ js/account.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $.ajax({ url: "http://rg.cape.io/templates/mini/account.html" })
   .done(function(template) {
     console.log("step #1");
-    $.ajax({ url: '/_api/db/_entity/users/'+$.cookie('uid')+'/profile.json'})
+    $.ajax({ url: '/_api/db/_entity/user/'+$.cookie('uid')+'/profile.json'})
     .done(function(user_info) {
       console.log(user_info);
       console.log(template);
