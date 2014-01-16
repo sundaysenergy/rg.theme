c685d052a6215d0e7f2b1d730a4575a531c1fa13
Tim Lupfer <tim@timlupfer.com>
2014-01-16 13:40:40 -0600

    [#63986126] Redirect if we don’t have a user token

--- js/account.js
+++ js/account.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
+  // Redirect to login page if we don't have a token
+  if (_.isUndefined($.cookie('token'))) window.location = '/trade/login.html#destination=' + encodeURIComponent(window.location.pathname);
+
+  // Fetch the template
   $.ajax({ url: "http://rg.cape.io/templates/mini/account.html" })
   .done(function(template) {
     var account_info = Hogan.compile(template);
+    // Fetch the user information
     $.ajax({ url: 'http://rg.cape.io/_api/db/_entity/user/'+$.cookie('uid')+'/profile.json'})
     .done(function(user_info) {
+      // Add the compiled and rendered template to the page
       $('.account-information').html(account_info.render(user_info));
+      // Set default to inline editing for bootstrap editable
       $.fn.editable.defaults.mode = 'inline';
+      // Make fields editable
       $('.account-information .editable').editable({
           type: 'select',
           ajaxOptions: {
