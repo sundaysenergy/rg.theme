ea7317bf031b6720749d0729afe5b68c5ffde528
Tim Lupfer <tim@timlupfer.com>
2014-01-17 16:13:09 -0600

    [#64076736] Notify CAPE of logout

--- logout.js
+++ logout.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
+  var token = $.cookie('token');
   $.removeCookie('uid', { path: '/', domain: '.rg.cape.io' });
   $.removeCookie('token', { path: '/', domain: '.rg.cape.io' });
-  window.location = document.referrer;
+
+  $.ajax({
+    url: 'http://rg.cape.io/logout',
+    type: 'GET',
+    headers: { Authorization: 'bearer '+token },
+    contentType: 'application/json',
+    success: function(result) {
+      window.location = document.referrer;
+    },
+    fail: function(result) {
+      window.location = document.referrer;
+    },
+    error: function(result) {
+      window.location = document.referrer;
+    }
+  });
 });
