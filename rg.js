39e581ac7c4b64de5e130770b7fa4591506deb57
Tim Lupfer <tim@timlupfer.com>
2014-01-17 14:46:54 -0600

    Check for token and remove disabled class

--- rg.js
+++ rg.js
@@ -1,13 +1,17 @@
 /* some stuff here */
-$('.fa-search').hover(function() {
-  $('#search-items').slideToggle();
-  $('#search-items').on('submit', function(e) {
-    e.preventDefault();
-    var collection = hash.get('collection');
-    if (_.isUndefined(collection)) collection = 'textile';
-    window.location = '/collection.html#collection='+collection+'&search=' + encodeURIComponent($('#search-items').find('input').val());
-  });
-  $('.fa-search').off('mouseenter');
-}, function() {
+$(document).ready(function() {
+  $('.fa-search').hover(function() {
+    $('#search-items').slideToggle();
+    $('#search-items').on('submit', function(e) {
+      e.preventDefault();
+      var collection = hash.get('collection');
+      if (_.isUndefined(collection)) collection = 'textile';
+      window.location = '/collection.html#collection='+collection+'&search=' + encodeURIComponent($('#search-items').find('input').val());
+    });
+    $('.fa-search').off('mouseenter');
+  }, function() {
 
+  });
+  var token = $.cookie('token');
+  if (_.isUndefined(token) == false) $('ul.trade-login').removeClass('disabled');
 });
