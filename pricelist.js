4484ba1b05c9bf24db820766eb16c44815645aba
Tim Lupfer <tim@timlupfer.com>
2014-01-20 15:55:59 -0600

    Debugging

--- pricelist.js
+++ pricelist.js
@@ -28,6 +28,8 @@ $.getJSON('http://rg.cape.io/items/client_data.json', function(data) {
     item.tradeprice = _.bind(itemPrice, item_prices, item.id);
   });
 
+  console.log(data);
+
   var template = Hogan.compile('{{#items}}<tr><td class="color">{{color}}</td><td class="content">{{content}}</td><td class="id">{{id}}</td><td class="name">{{name}}</td><td class="tradeprice">{{tradeprice}}</td><td class="repeat">{{repeat}}</td><td class="width">{{width}}</td></tr>{{/items}}'); 
   $('tbody.list').hide();
   $('tbody.list').html(template.render(data));
