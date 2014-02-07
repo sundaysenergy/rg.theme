56182eeab6d27b367b5b0cd5c15d81a65f49df49
Tim Lupfer <tim@timlupfer.com>
2014-02-07 15:19:05 -0600

    [#65391448] Remove click handlers when returning to horizontal view in ie9

--- productview.js
+++ productview.js
@@ -727,7 +727,7 @@ $(document).ready(function() {
         }
         // For each visible li in the list, create a click handler that toggles visibility
         // and compiles the mustache for the current item.
-        $('ul.slider li, ul.list li').off('click touch');
+        $('ul.slider li, ul.list li .img').off('click touch');
         $('ul.slider li .item-spotlight').off('click touch').on('click touch', function(e) {
           if (e.target !== this) {
             console.log("Not the right element");
