ba4f4f11ee2053d4f80a6c303c40f5a85932a687
Tim Lupfer <tim@timlupfer.com>
2014-02-07 15:13:17 -0600

    Fix bug when moving back and forth between view modes in ie9

--- productview.js
+++ productview.js
@@ -727,9 +727,12 @@ $(document).ready(function() {
         }
         // For each visible li in the list, create a click handler that toggles visibility
         // and compiles the mustache for the current item.
-        $('ul.slider li:nth-of-type(2)').off('click touch');
-        $('ul.slider li .item-spotlight').off('click touch').on('click touch', function(e) {
-          if (e.target !== this) return true;
+        $('ul.slider li, ul.list li').off('click touch');
+        $('ul.slider li .item-spotlight, ul.slider').off('click touch').on('click touch', function(e) {
+          if (e.target !== this) {
+            console.log("Not the right element");
+            return true;
+          }
           var id = $(this).parent().find('.id').html();
           hash.add({detailedview:id});
         });
