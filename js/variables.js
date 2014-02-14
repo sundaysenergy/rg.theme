0491426a9c73196cd935776ec9c9aaab5b6331fc
Tim Lupfer <tim@timlupfer.com>
2014-02-14 13:16:52 -0600

    Change url for api thing

--- js/variables.js
+++ js/variables.js
@@ -1,6 +1,6 @@
 var rg_options = {
-  api: 'http://rg.cape.io',
-  cookiedomain: '.rg.cape.io',
+  api: 'http://r-g.cape.io',
+  cookiedomain: '.r-g.cape.io',
   vertical_page: 42,
   horizontal_page: 3
 }
