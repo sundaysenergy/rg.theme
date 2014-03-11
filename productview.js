4f8d65a520119dab4c6baac0852f39187ce89a2c
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-03-11 05:30:03 -0400

    let's see how this works sans js styling

--- productview.js
+++ productview.js
@@ -752,7 +752,8 @@ $(document).ready(function() {
           $('ul.slider > li').height(slideHeight);
           $('ul.slider > li > img').width(slideWidth);
         }
-      } else {
+      } else {/*
+
         $('ul.passementerie li:visible .img:visible,ul.passementerie li:visible').attr('style','');
         var maxheight = $('ul.passementerie.slider li:visible:nth-of-type(2)').find('.img').height();
         //console.log(maxheight);
@@ -765,6 +766,7 @@ $(document).ready(function() {
           }
           var margin = ($li.height() / 2) - ($img.height() / 2);
           $li.css('margin-top',margin).height($li.height()-margin);
+*/
         });
       }
     });
