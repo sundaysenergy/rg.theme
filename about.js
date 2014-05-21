6fea83b56740c2fefb2553e68dee0af65624ee81
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-05-21 10:28:57 -0400

    better? worse?

--- about.js
+++ about.js
@@ -8,3 +8,31 @@ $('#photostream').width(ww).css('margin-left', (ww/2)*-1);
 $( '#slider1' ).lemmonSlider({
   'infinite' : true
 });
+
+$(window).resize(function() {
+  var ww = $(window).width();
+  $('#photostream').width(ww).css('margin-left', (ww/2)*-1);
+});
+
+$('.carousel-indicators').on('click','li',function() {
+  $(this).toggleClass('active');
+  $(this).siblings().removeClass('active');
+});
+
+var index,
+total=$('.slider li').not('.-before, .-after').length;
+$('<p class="pagination"></p>').appendTo('#photostream');
+
+var fun = function (e){
+  $('.pagination').text('');
+  
+  var $li = $('.slider li').not('.-before, .-after');
+  index = $li.filter('.active').index()-15;
+  console.log(index);
+  index = (index == -16)?1:index;
+  
+  $('.pagination').append(index +'/'+ total);
+  
+}
+
+$('.next-slide, .next-page, .prev-page, .prev-slide').click(fun);
