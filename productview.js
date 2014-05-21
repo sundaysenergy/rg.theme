1c35ca9d6be8ff25937812806861bbde9f795b71
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-05-21 15:20:51 -0400

    also trigger on hash change?

--- productview.js
+++ productview.js
@@ -272,6 +272,34 @@ $(document).ready(function() {
     /**** THINGS TO DO WHEN THE HASH CHANGES ****/
     $(window).on('hashchange', function(e) {
 
+      var $anon = $('#products ul.list');
+      var anontxt = $('#anon-fav-text');
+      var maintxt = $('#main-text');
+      var favtxt_shown = false;
+    
+      var show_favtxt = function() {
+        if ($anon.hasClass('anon-favorites')) {
+          console.log('show favtxt');
+          anontxt.show();
+          maintxt.hide();
+          favtxt_shown = true;
+        }
+        else if (favtxt_shown) {
+          console.log('hide favtxt');
+          anontxt.hide();
+          maintxt.show();
+          favtxt_shown = false;
+        }
+        else {
+          console.log('hide favtxt');
+          anontxt.hide();
+          maintxt.show();
+          favtxt_shown = false;
+        }
+      }
+  
+      show_favtxt();
+
       // Create a string that contains parts of the hash that indicates need to refilter
       var current_page = _.map(_.pull(_.keys(hash.get()), 'pos', 'detailedview', 'cpos', 'dpos'), function(key) { return key + '=' + hash.get(key) }).join('&');
       
@@ -1394,7 +1422,7 @@ $(document).ready(function() {
         favtxt_shown = false;
       }
     }
-  
+
     show_favtxt();
 
   });
