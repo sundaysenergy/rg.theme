8527ee82bf5259fe94840c3c8fbb4d3ee954a2ea
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-05-21 13:40:50 -0400

    return SS to orig
    remove some extra spaces

--- lemmon-slider-centered.js
+++ lemmon-slider-centered.js
@@ -1,7 +1,8 @@
+// from here: http://jquery.lemmonjuice.com/plugins/slider-variable-widths.php
+
 (function( $ ) {
 
   var _css = {};
-
   var methods = {
 
     // Initialzie plugin
@@ -46,7 +47,6 @@
 
           // first item
           //$items.filter( ':first' ).addClass( 'active' );
-
           slideTo( {}, $slider, 0, 0, 0 );
 
           // attach events
@@ -99,6 +99,62 @@
             }
 
           } );
+          $slider.bind( 'nextPage', function( e, t ) {
+
+            var scroll = $slider.scrollLeft();
+            var w = $slider.width();
+            var x = 0;
+            var slide = 0;
+
+            $items.each( function( i ) {
+              if( $( this ).position().left < w ) {
+                x = $( this ).position().left;
+                slide = i;
+              }
+            } );
+
+            if( x > 0 && scroll + w + 1 < originalWidth ) {
+              slideTo( e, $slider, scroll + x, slide, 'slow' );
+            } else if( options.loop ) {
+              // return to first
+              slideTo( e, $slider, 0, 0, 'slow' );
+            }
+
+          } );
+          $slider.bind( 'prevPage', function( e, t ) {
+
+            var scroll = $slider.scrollLeft();
+            var w = $slider.width();
+            var x = 0;
+
+            $items.each( function( i ) {
+              if( $( this ).position().left < 1 - w ) {
+                x = $( this ).next().position().left;
+                slide = i;
+              }
+            } );
+
+            if( scroll ) {
+              if( x == 0 ) {
+                //$slider.animate({ 'scrollLeft' : 0 }, 'slow' );
+                slideTo( e, $slider, 0, 0, 'slow' );
+              } else {
+                //$slider.animate({ 'scrollLeft' : scroll + x }, 'slow' );
+                slideTo( e, $slider, scroll + x, slide, 'slow' );
+              }
+            } else if( options.loop ) {
+              // return to last
+              var a = $sliderContainer.outerWidth() - $slider.width();
+              var b = $items.filter( ':last' ).position().left;
+              if( a > b ) {
+                $slider.animate( { 'scrollLeft': b }, 'slow' );
+              } else {
+                $slider.animate( { 'scrollLeft': a }, 'slow' );
+              }
+            }
+
+          } );
+          
           $slider.bind( 'slideTo', function( e, i, t ) {
 
             slideTo(
@@ -117,9 +173,16 @@
             $slider.trigger( 'prevSlide' );
             return false;
           } );
+          $sliderControls.find( '.next-page' ).click( function() {
+            $slider.trigger( 'nextPage' );
+            return false;
+          } );
+          $sliderControls.find( '.prev-page' ).click( function() {
+            $slider.trigger( 'prevPage' );
+            return false;
+          } );
 
           //if ( typeof $slider.options.create == 'function' ) $slider.options.create();
-
           $slider.data( 'slider', {
             'target':  $slider,
             'options': options
@@ -162,16 +225,20 @@
       return this.each( function() {
 
         var $slider = $( this ),
-            $sliderControls = $slider.next().filter( '.controls' ),
-            $items = $slider.find( '> *:first > *' ),
-            data = $slider.data( 'slider' );
+          $sliderControls = $slider.next().filter( '.controls' ),
+          $items = $slider.find( '> *:first > *' ),
+          data = $slider.data( 'slider' );
 
         $slider.unbind( 'nextSlide' );
         $slider.unbind( 'prevSlide' );
+        $slider.unbind( 'nextPage' );
+        $slider.unbind( 'prevPage' );
         $slider.unbind( 'slideTo' );
 
         $sliderControls.find( '.next-slide' ).unbind( 'click' );
         $sliderControls.find( '.prev-slide' ).unbind( 'click' );
+        $sliderControls.find( '.next-page' ).unbind( 'click' );
+        $sliderControls.find( '.next-page' ).unbind( 'click' );
 
         $slider.removeData( 'slider' );
 
@@ -183,7 +250,6 @@
       } );
 
     }
-
   }
 
   // Private functions
@@ -236,13 +302,10 @@
   }
 
   // Debug
-  //
   function debug( text ) {
     $( '#debug span' ).text( text );
   }
 
-  //
-  //
   $.fn.lemmonSlider = function( method, options ) {
     if( methods[method] ) {
       return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
@@ -254,10 +317,7 @@
 
   };
 
-  //
-  //
   $.fn.lemmonSlider.defaults = {
-
     'items':       '> *',
     'loop':        true,
     'slideToLast': false,
@@ -266,7 +326,6 @@
     'infinite':    true,
     'center':      true,
     'offset':      0
-
   }
 
 })( jQuery );
