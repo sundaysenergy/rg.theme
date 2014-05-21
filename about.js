81f25bf9a19ef68128bcd89703ea06792503f83c
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-05-21 00:05:51 -0400

    here is a start with some css and js

--- about.js
+++ about.js
@@ -1,1 +1,9 @@
 // commenting to see if this works
+var ww = $(window).width();
+$('#photostream').width(ww).css('margin-left', (ww/2)*-1);
+
+var uno    = $("#photostream").find(".uno");
+var dos    = $("#photostream").find(".dos");
+var tres   = $("#photostream").find(".tres");
+var quatro = $("#photostream").find(".quatro");
+var cinco  = $("#photostream").find(".cinco");
