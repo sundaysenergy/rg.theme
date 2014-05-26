fa9090b7478e5e38f3e118bd2ebcbf34189fb2f9
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-05-25 22:41:48 -0400

    wtf

--- about.js
+++ about.js
@@ -6,21 +6,25 @@ $('#photostream').width(ww).css('margin-left', (ww/2)*-1);
 // lemmon
 // came from http://jquery.lemmonjuice.com/plugins/slider-variable-widths.php and then customized from there
 $( '#slider1' ).lemmonSlider({
-  'infinite' : true
+  'infinite' : true,
 });
+/*
 
 $('.carousel-indicators').on('click','li',function() {
   $(this).addClass('active');
   $(this).siblings().removeClass('active');
 });
 
+*/
 $(window).resize(function() {
   var ww = $(window).width();
   $('#photostream').width(ww).css('margin-left', (ww/2)*-1);
 });
 
+// some other ideas
 var index;
-var total = $('.slider li').not('.-before, .-after').length;
+var $li = $('.slider li').not('.-before, .-after');
+var total = $li.length;
 var tbefore = $('.slider li').filter('.-before').length;
 var tafter = $('.slider li').filter('.-after').length;
 
@@ -28,7 +32,5 @@ console.log(total);
 console.log(tbefore);
 console.log(tafter);
 
-var $li = $('.slider li').not('.-before, .-after');
 index = $li.filter('.active').index();
 console.log(index-tbefore);
-
