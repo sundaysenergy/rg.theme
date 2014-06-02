3faa96b6c34dd67bd3f2a7cd3574be8d34a6cd9e
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-06-02 15:45:13 -0400

    update about page

--- about.js
+++ about.js
@@ -1,5 +1,6 @@
 
 // commenting to see if this works
+/*
 var ww = $(window).width();
 $('#photostream').width(ww).css('margin-left', (ww/2)*-1);
 
@@ -18,17 +19,115 @@ $(window).resize(function() {
   var ww = $(window).width();
   $('#photostream').width(ww).css('margin-left', (ww/2)*-1);
 });
+*/
 
-// some other ideas
-var index;
-var $li = $('.slider li').not('.-before, .-after');
-var total = $li.length;
-var tbefore = $('.slider li').filter('.-before').length;
-var tafter = $('.slider li').filter('.-after').length;
+// old is above here and will eventually be deleted
 
-console.log(total);
-console.log(tbefore);
-console.log(tafter);
+// new totally custom stuff below here...
 
-index = $li.filter('.active').index();
-console.log(index-tbefore);
+var aboutss = function() {
+  var ww = $(window).width();
+  $('#photostream').width(ww).css('margin-left', (ww/2)*-1);
+  
+  var orig_li = $('#photostream ul li.orig');
+  var accum_width = 0;
+  
+  // individual image sizes
+  // how do I make this dynamic?
+  var slot1 = orig_li.eq(0).width() + 5;
+  var slot2 = orig_li.eq(1).width() + 5;
+  var slot3 = orig_li.eq(2).width() + 5;
+  var slot4 = orig_li.eq(3).width() + 5;
+  var slot5 = orig_li.eq(4).width() + 5;
+  
+  orig_li.find('img').each(function() {
+     accum_width += $(this).width() + 5;
+  });
+  
+  $('#photostream ul').width(accum_width * 3).css('margin-left', ((accum_width * 3)/2)*-1);
+  
+  orig_li.clone().addClass('-after').removeClass('orig').insertAfter( orig_li.filter( ':last' ) );
+  orig_li.filter( ':first' ).before( orig_li.clone().addClass( '-before' ).removeClass('orig') );
+  orig_li.eq(0).addClass('active');
+  
+  // reposition
+  var center1 = accum_width + (slot1/2);
+  var center2 = accum_width + slot1 + (slot2/2);
+  var center3 = accum_width + slot1 + slot2 + (slot3/2);
+  var center4 = accum_width + slot1 + slot2 + slot3 + (slot4/2);
+  var center5 = accum_width + slot1 + slot2 + slot3 + slot4 + (slot5/2);
+  
+  // center on actual active one...
+  var center_on_active = function() {
+    var index;
+    var total = orig_li.length;
+    var tbefore = $('.slider li').filter('.-before').length;
+    var tafter = $('.slider li').filter('.-after').length;
+    
+        index = orig_li.filter('.active').index();
+    
+    var active_one = index-tbefore;
+  
+    if (active_one === 0 ) {
+      $('#photostream ul').css('margin-left', (center1*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 1 ) {
+      $('#photostream ul').css('margin-left', (center2*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 2 ) {
+      $('#photostream ul').css('margin-left', (center3*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 3 ) {
+      $('#photostream ul').css('margin-left', (center4*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 4 ) {
+      $('#photostream ul').css('margin-left', (center5*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    else {}
+  };
+  
+  center_on_active();
+  
+  //okay prev/next actions...
+  var next_slide       = $('.controls .next-slide');
+  var prev_slide       = $('.controls .prev-slide');
+  
+  next_slide.click(function(e) {
+    e.preventDefault();
+    var active_slide     = $('#photostream ul li.active');
+    var active_indicator = $('.carousel-indicators li.active');
+    active_slide.removeClass('active').next('li').addClass('active');
+    center_on_active();
+  });
+  
+  prev_slide.click(function(e) {
+    e.preventDefault();
+    var active_slide     = $('#photostream ul li.active');
+    var active_indicator = $('.carousel-indicators li.active');
+    active_slide.removeClass('active').prev('li').addClass('active');
+    center_on_active();
+  });
+  
+  // oh crap — what to do when the last or first is reached?
+  
+  
+  // okay... now to figure out the indicators.
+  // how do I make all the indicators print out automatically based on images that are hard coded?
+  $('.carousel-indicators').on('click','li',function() {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  });
+  // end slideshow nonsense
+};
+
+aboutss();
+
+// fix things up on resize...
+$(window).resize(function() {
+  aboutss();
+});
