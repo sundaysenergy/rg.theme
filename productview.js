0449150cfa14a348368a237cb64eedebb97f7299
Tim Lupfer <tim@timlupfer.com>
2014-06-02 19:26:37 -0500

    No border on the collection header thing when in textile or passementerie

--- productview.js
+++ productview.js
@@ -219,7 +219,7 @@ $(document).ready(function() {
       if (_.isUndefined(collection) == false && _.isUndefined(srch)) {
         /*** TEXTILE COLLECTION ***/
         if (collection == 'textile') {
-          $('#collection-headers-after').css('border-bottom','0').css('border-top','1px solid #262019');
+          $('#collection-headers-after').css('border-bottom','0').css('border','0');
           $('#collection-headers-after').removeClass('border-top');
           $('#products,#collection-menu-main,#collection-menu-leather-inactive,#collection-menu-passementerie-inactive').show();
           if (productlist.page == rg_options.horizontal_page) {
@@ -238,7 +238,7 @@ $(document).ready(function() {
         }
         /*** PASSEMENTERIE COLLECTION ***/
         if (collection == 'passementerie') {
-          $('#collection-headers-after').css('border-bottom','0').css('border-top','1px solid #262019');
+          $('#collection-headers-after').css('border-bottom','0').css('border','0');
           $('#collection-headers-after').removeClass('border-top');
           $('.collection-view-items').show();
           $('.collection-view-items a').each(function(i) {
