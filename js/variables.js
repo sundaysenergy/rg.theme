24f837f6846ce570c799153d983b8dd83056a353
Kai Curry <kai@sundaysenergy.com>
2014-06-05 17:13:27 -0500

    must be same domain

--- js/variables.js
+++ js/variables.js
@@ -1,6 +1,6 @@
 var rg_options = {
   api: 'http://r_g.cape.io',
-  cdn: 'http://cdn.rogersandgoffigon.com',
+  cdn: 'http://www.rogersandgoffigon.com',
   cookiedomain: '.r_g.cape.io',
   vertical_page: 42,
   horizontal_page: 3
