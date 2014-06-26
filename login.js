7b314d03784ef0304562eb3c6f3d43bebda3d9d0
Kai Curry <kai@sundaysenergy.com>
2014-06-26 17:40:32 -0500

    post to _login instead of /login

--- login.js
+++ login.js
@@ -4,11 +4,8 @@ $(document).ready(function() {
     e.preventDefault();
     var username = $('input[type=email]').val();
     var password = $('input[type=password]').val();
-    var posting = $.post(rg_options.api + '/login',
-                          {
-                            username: username,
-                            password: password
-                          },
+    var posting = $.post(rg_options.api + '/_login',
+                          { username: username, password: password },
                           function(data) {
                             $.cookie('token', data.token, { expires: 1, path: '/', domain: rg_options.cookiedomain });
                             $.cookie('uid', data.uid, { expires: 1, path: '/', domain: rg_options.cookiedomain });
