b35f5f7a8df74ad93223c82428b2f56b027a4a9c
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-06-26 13:05:00 -0400

    a bunch more crap into about.js that I need help figuring out how to systematize

--- about.js
+++ about.js
@@ -10,11 +10,17 @@ var aboutss = function() {
 
   // individual image sizes
   // any way to do this dynamically so if the number of slides changes this doesn't have to be rewritten?
-  var slot1 = orig_li.eq(0).width() + 5;
-  var slot2 = orig_li.eq(1).width() + 5;
-  var slot3 = orig_li.eq(2).width() + 5;
-  var slot4 = orig_li.eq(3).width() + 5;
-  var slot5 = orig_li.eq(4).width() + 5;
+  var slot1  = orig_li.eq(0).width() + 5;
+  var slot2  = orig_li.eq(1).width() + 5;
+  var slot3  = orig_li.eq(2).width() + 5;
+  var slot4  = orig_li.eq(3).width() + 5;
+  var slot5  = orig_li.eq(4).width() + 5;
+  var slot6  = orig_li.eq(5).width() + 5;
+  var slot7  = orig_li.eq(6).width() + 5;
+  var slot8  = orig_li.eq(7).width() + 5;
+  var slot9  = orig_li.eq(8).width() + 5;
+  var slot10 = orig_li.eq(9).width() + 5;
+  var slot11 = orig_li.eq(10).width() + 5;
 
   orig_li.find('img').each(function() {
      accum_width += $(this).width() + 5;
@@ -28,13 +34,19 @@ var aboutss = function() {
 
   // reposition
   // any way to do this dynamically so if the number of slides changes this doesn't have to be rewritten?
-  var center0 = accum_width - (slot5/2);
-  var center1 = accum_width + (slot1/2);
-  var center2 = accum_width + slot1 + (slot2/2);
-  var center3 = accum_width + slot1 + slot2 + (slot3/2);
-  var center4 = accum_width + slot1 + slot2 + slot3 + (slot4/2);
-  var center5 = accum_width + slot1 + slot2 + slot3 + slot4 + (slot5/2);
-  var center6 = accum_width + slot1 + slot2 + slot3 + slot4 + slot5 + (slot1/2);
+  var center0  = accum_width - (slot5/2);
+  var center1  = accum_width + (slot1/2);
+  var center2  = accum_width + slot1 + (slot2/2);
+  var center3  = accum_width + slot1 + slot2 + (slot3/2);
+  var center4  = accum_width + slot1 + slot2 + slot3 + (slot4/2);
+  var center5  = accum_width + slot1 + slot2 + slot3 + slot4 + (slot5/2);
+  var center6  = accum_width + slot1 + slot2 + slot3 + slot4 + slot5 + (slot6/2);
+  var center7  = accum_width + slot1 + slot2 + slot3 + slot4 + slot5 + slot6 + (slot7/2);
+  var center8  = accum_width + slot1 + slot2 + slot3 + slot4 + slot5 + slot6 + slot7 + (slot8/2);
+  var center9  = accum_width + slot1 + slot2 + slot3 + slot4 + slot5 + slot6 + slot7 + slot8 + (slot9/2);
+  var center10 = accum_width + slot1 + slot2 + slot3 + slot4 + slot5 + slot6 + slot7 + slot8 + slot9 + (slot10/2);
+  var center11 = accum_width + slot1 + slot2 + slot3 + slot4 + slot5 + slot6 + slot7 + slot8 + slot9 + slot10 + (slot11/2);
+  var center12 = accum_width + slot1 + slot2 + slot3 + slot4 + slot5 + slot6 + slot7 + slot8 + slot9 + slot10 + slot11 +(slot12/2);
 
   // center on actual active one...
   // any way to do this dynamically so if the number of slides changes this doesn't have to be rewritten?
@@ -66,6 +78,30 @@ var aboutss = function() {
     }
     if (active_one === 4 ) {
       $('#photostream ul').css('margin-left', (center5*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 5 ) {
+      $('#photostream ul').css('margin-left', (center6*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 6 ) {
+      $('#photostream ul').css('margin-left', (center7*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 7 ) {
+      $('#photostream ul').css('margin-left', (center8*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 8 ) {
+      $('#photostream ul').css('margin-left', (center9*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 9 ) {
+      $('#photostream ul').css('margin-left', (center10*-1));
+      $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
+    }
+    if (active_one === 10 ) {
+      $('#photostream ul').css('margin-left', (center11*-1));
       $('.carousel-indicators li').eq(active_one).addClass('active').siblings().removeClass('active');
     }
     else {}
