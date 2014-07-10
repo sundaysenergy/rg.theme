a8a1d2c8939de6cefb22ca4dc274c82bf19abf55
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-07-10 15:38:53 -0400

    i don't think this is used anymore? hardcoded into the script file instead…
    should we delete or fix how this works?

--- productview.js
+++ productview.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
         item_passementerie           = Hogan.compile(templates.item_passementerie),
         item_mobile_template         = Hogan.compile(templates.item_mobile),
         spotlight_template           = Hogan.compile(templates.spotlight),
-        spotlight_pass_template      = Hogan.compile(templates.spotlight_passementerie),
+        spotlight_pass_template      = Hogan.compile(templates.spotlight_passementerie), //is this in use anymore?
         dummy_template               = Hogan.compile(templates.bookends),
         dummy_template_pass          = Hogan.compile(templates.bookends_passementerie),
         favorites_template           = Hogan.compile(templates.favorites),
