42eb66208bd82f9ae718366b4b6f7acfe5ec43c1
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-07-15 20:06:02 -0400

    attempting to add fix from github

--- about.js
+++ about.js
@@ -3,4 +3,21 @@ $(function() {
     container: '#film_roll',
     no_css: true
   });
+
+//Enable swiping...
+  $("#film_roll").swipe( {
+    //Generic swipe handler for all directions
+    swipe:function(event, direction, distance, duration, fingerCount) {
+      if (direction=="left") { //swipe left
+        var i = (film_roll.index+1) % film_roll.children.length;
+        film_roll.moveToIndex(i, 'left');
+      } else { //swipe right
+        var i = (film_roll.index == 0 ? film_roll.children.length : film_roll.index) - 1;
+        film_roll.moveToIndex(i, 'right');
+      }
+    },
+    //Default is 75px, set to 0 for demo so any distance triggers swipe
+     threshold:0
+  });
+
 });
