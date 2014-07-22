27a386e7cdf6272ca1b9ae40350a83e9b9cc04e4
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-07-22 01:08:33 -0400

    remove bad idea

--- productview.js
+++ productview.js
@@ -1316,11 +1316,6 @@ $(document).ready(function() {
               $('#item-colors').remove();
               $('.item-icons').find('.active').removeClass( 'active' );
             });
-            if ( $(this).hasClass('active') === true) {
-              $('.item-passementerie').remove();
-              $('#item-colors').remove();
-              $('.item-icons').find('.active').removeClass( 'active' );
-            } else {}
           });
           
           // passementerie color related stuff
@@ -1399,11 +1394,6 @@ $(document).ready(function() {
               $('.item-passementerie').remove();
               $('.item-icons').find('.active').removeClass( 'active' );
             });
-            if ( $(this).hasClass('active') === true) {
-              $('.item-passementerie').remove();
-              $('#item-colors').remove();
-              $('.item-icons').find('.active').removeClass( 'active' );
-            } else {}
           });
         }
       });
